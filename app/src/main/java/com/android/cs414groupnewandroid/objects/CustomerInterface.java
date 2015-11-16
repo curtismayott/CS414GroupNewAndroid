6279472f3b90cc9d62019429a7e917a9c391871e
darkbobo <curtis.mayott@gmail.com>
2015-11-15 20:57:02 -0700

    lipeRMI plugged in

---
+++ app/src/main/java/com/android/cs414groupnewandroid/objects/CustomerInterface.java
@@ -0,0 +1,10 @@
+package com.android.cs414groupnewandroid.objects;
+
+/**
+ * Created by darkbobo on 11/15/15.
+ */
+public interface CustomerInterface {
+	public Person getCustomer();
+	public void editCustomer(Person person);
+	public void addCustomer(Person person);
+}
--- app/src/main/java/com/android/cs414groupnewandroid/objects/Order.java
+++ app/src/main/java/com/android/cs414groupnewandroid/objects/Order.java
@@ -13,7 +13,7 @@ TODO functionality of updating order : have totalPaid, when user edits pizza, to
 */
 
 
-public class Order {
+public class Order implements OrderInterface {
     private int orderID;
     private Person customer;
     boolean isPaidFor;
@@ -259,4 +259,9 @@ public class Order {
     public ORDER_TYPE getOrderType(){ return this.orderType; }
 
     public void setOrderType(ORDER_TYPE orderType){ this.orderType = orderType; }
+
+	@Override
+	public void sendOrder() {
+
+	}
 }
