5a484700b8b02ea0f72a1643572f9f3d157b9e42
darkbobo <curtis.mayott@gmail.com>
2015-11-22 00:50:51 -0700

    now customer information is required correctly and clears correctly when navigate away from screen

--- app/src/main/java/com/android/cs414groupnewandroid/controllers/CustomerListener.java
+++ app/src/main/java/com/android/cs414groupnewandroid/controllers/CustomerListener.java
@@ -1,14 +1,18 @@
 package com.android.cs414groupnewandroid.controllers;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.android.cs414groupnewandroid.R;
 import com.android.cs414groupnewandroid.activities.MainActivity;
 import com.android.cs414groupnewandroid.objects.Address;
 import com.android.cs414groupnewandroid.objects.ORDER_TYPE;
@@ -62,21 +66,67 @@ public class CustomerListener extends MyOnClickListener implements AdapterView.O
                 order.setOrderType(ORDER_TYPE.DELIVERY);
             }
 
-            System.out.println("OrderID: " + orderID);
-            MainActivity.passOrderID(MainActivity.ORDER_EDIT, orderID);
-            MainActivity.changeScreen(MainActivity.ORDER_EDIT);
-            orderID = -1;
-            order = null;
+			if(!verifyFields()) {
+				System.out.println("OrderID: " + orderID);
+				clearEditTextFields();
+				MainActivity.passOrderID(MainActivity.ORDER_EDIT, orderID);
+				MainActivity.changeScreen(MainActivity.ORDER_EDIT);
+				orderID = -1;
+				order = null;
+			}else{
+				showAlertFields();
+			}
         }
     }
+	public boolean verifyFields(){
+		boolean showDialog = false;
+		if(order.getOrderType() == ORDER_TYPE.CARRY_OUT &&
+				(order.getCustomer().getName().equals("")
+						|| order.getCustomer().getPhoneNumbers().get(0).getNumber().equals(""))
+				){
+			showDialog = true;
+		}else if(order.getOrderType() == ORDER_TYPE.DELIVERY &&
+				(order.getCustomer().getName().equals("")
+						|| order.getCustomer().getAddress(0).getCity().equals("")
+						|| order.getCustomer().getAddress(0).getState().equals("")
+						|| order.getCustomer().getAddress(0).getZipcode().equals("")
+						|| order.getCustomer().getAddress(0).getStreetAddress().equals("")
+				)
+				){
+			showDialog = true;
+		}else if(order.getOrderType() == ORDER_TYPE.PICK_UP && order.getCustomer().getName().equals("")){
+			showDialog = true;
+		}
+		return showDialog;
+	}
+	public void showAlertFields(){
+		final Dialog fieldRequire = new Dialog(context);
+		fieldRequire.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		fieldRequire.setContentView(R.layout.dialog_cancel);
+		((TextView)fieldRequire.findViewById(R.id.label)).setText("All of the fields must have information.");
+		Button cancel = (Button)fieldRequire.findViewById(R.id.cancel_button);
+		cancel.getLayoutParams().width = LinearLayout.LayoutParams.FILL_PARENT;
+		Button confirm = (Button)fieldRequire.findViewById(R.id.confirm_button);
+		confirm.setVisibility(View.GONE);
+		cancel.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				fieldRequire.dismiss();
+			}
+		});
+		fieldRequire.show();
+	}
 
     public void clearEditTextFields(){
-        ((EditText) components.get("phoneEditText")).setText("");
-        ((EditText) components.get("nameEditText")).setText("");
-        ((EditText) components.get("streetEditText")).setText("");
-        ((EditText) components.get("cityEditText")).setText("");
-        ((EditText) components.get("stateEditText")).setText("");
-        ((EditText) components.get("zipEditText")).setText("");
+		((EditText) components.get("phoneEditText")).setText("");
+		((EditText) components.get("nameEditText")).setText("");
+		((EditText) components.get("streetEditText")).setText("");
+		((EditText) components.get("cityEditText")).setText("");
+		((EditText) components.get("stateEditText")).setText("");
+		((EditText) components.get("zipEditText")).setText("");
+		((Spinner) components.get("orderTypeSpinner")).setSelection(((ArrayAdapter) ((Spinner) components.get("orderTypeSpinner")).getAdapter()).getPosition("Pickup"));
+		(components.get("addressContainer")).setVisibility(View.GONE);
+		(components.get("phoneContainer")).setVisibility(View.GONE);
     }
 
     @Override
